16cb472f84a04f118858f66ceda4c1ced5e1e93b
Janice199x <janice.nkuna@gmail.com>
2023-12-18 12:41:53 -0800

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,7 +5,7 @@
  */
 
 int main(void)
-(
-         prinf("with proper grammer, but the outcome is a piece of art,\n")
-	 Return (0);
-)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
