977ea028fe0662effc3b36e6003de9c05ea89a75
Janice199x <janice.nkuna@gmail.com>
2023-12-20 01:48:25 -0800

    100-times_table.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -14,7 +14,7 @@ int _abs(int);
 
 int print_last_digit(int);
 
-void jack_baver(void);
+void jack_bauer(void);
 
 void times_table(void);
 
@@ -22,4 +22,4 @@ int add(int, int);
 
 void print_to_98(int n);
 
-void print_times_tables(int n);
+void print_times_table(int n);
