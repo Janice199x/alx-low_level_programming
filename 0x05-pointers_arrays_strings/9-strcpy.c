5733663c16ee8165e00fbba0c0add88f68aa75ed
Janice199x <janice.nkuna@gmail.com>
2024-01-02 12:13:29 -0800

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * char *_strcpy - a function that couples the string pointed to by src
+ * @dest: copy tp
+ * @src: copy from
+ * Return: string
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int l = 0;
+	int x = 0;
+
+	while (*(src + l) != '\0')
+	{
+		l++;
+	}
+	for ( ; x < 1 ; x++)
+	{
+		dest[x] = src[x];
+	}
+	dest[1] = '\0';
+	return (dest);
+}
